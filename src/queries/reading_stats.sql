df9ddf0d6019cf757fd755031bcc99733e230c08
firebirdsystems <admin@firebirdsystems.net>
2026-07-05 05:34:07 -0600

    reading_stats query

--- src/queries/reading_stats.sql
+++ src/queries/reading_stats.sql
@@ -2,7 +2,7 @@ SELECT
   b.member_id,
   COUNT(*)                                                        AS total_books,
   COALESCE(SUM(b.pages), 0)                                       AS total_pages,
-  ROUND(AVG(b.rating) FILTER (WHERE b.rating IS NOT NULL), 1)     AS avg_rating,
+  ROUND(AVG(b.rating), 1)                                         AS avg_rating,
   MAX(b.finished_at)                                              AS last_finished
 FROM app_reading_log__books b
 GROUP BY b.member_id
